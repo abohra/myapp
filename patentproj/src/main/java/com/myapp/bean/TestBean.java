095a99db6edac0a2cc8bf481fa0306bced0e0234
abohra <bohra.a@gmail.com>
2013-01-13 20:31:00 +0530

    Test example using spring for REST based service

---
+++ patentproj/src/main/java/com/myapp/bean/TestBean.java
@@ -0,0 +1,24 @@
+package com.myapp.bean;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement (name="test-bean")
+public class TestBean {
+	
+	private String object1;
+	private String object2;
+	
+	public String getObject1() {
+		return object1;
+	}
+	public void setObject1(String object1) {
+		this.object1 = object1;
+	}
+	public String getObject2() {
+		return object2;
+	}
+	public void setObject2(String object2) {
+		this.object2 = object2;
+	}
+
+}
