6c43598ba022d1005c42755aa452078479a65b55
abohra <bohra.a@gmail.com>
2013-01-26 15:00:31 +0530

    context file changes

--- patentproj/src/main/java/com/patent/bean/Customer.java
+++ patentproj/src/main/java/com/patent/bean/Customer.java
@@ -1,5 +1,6 @@
 package com.patent.bean;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 import javax.xml.bind.annotation.XmlRootElement;
@@ -13,35 +14,41 @@ public class Customer {
 	private String emailAddress;
 	private String contactNumber;
 	private String address;
+
+	@Column
 	public String getCustomerId() {
 		return customerId;
 	}
 	public void setCustomerId(String customerId) {
 		this.customerId = customerId;
 	}
+	@Column
 	public String getCustomerName() {
 		return customerName;
 	}
 	public void setCustomerName(String customerName) {
 		this.customerName = customerName;
 	}
+	@Column
 	public String getEmailAddress() {
 		return emailAddress;
 	}
 	public void setEmailAddress(String emailAddress) {
 		this.emailAddress = emailAddress;
 	}
+	@Column
 	public String getContactNumber() {
 		return contactNumber;
 	}
 	public void setContactNumber(String contactNumber) {
 		this.contactNumber = contactNumber;
 	}
+	@Column
 	public String getAddress() {
 		return address;
 	}
 	public void setAddress(String address) {
 		this.address = address;
 	}
-	
+
 }
